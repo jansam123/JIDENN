7d3a155dc93134d97421a684e07734e3bc80aa78
jansam123 <samueljankovych@gmail.com>
2023-09-10 14:54:15 +0200

    refactor

--- plot_phys.py
+++ plot_phys.py
@@ -3,24 +3,26 @@ import matplotlib.pyplot as plt
 from puma import Histogram, HistogramPlot
 import pandas as pd
 import numpy as np
+import os
 import atlasify
 
 from jidenn.data.JIDENNDataset import JIDENNDataset
 
-def sns_label_plotting(df):
+
+def sns_label_plotting(df, save_path):
     pt_name = r"$p_{\mathrm{T}}$ [TeV]"
     label_name = "label"
     bins = list(np.linspace(0.06, 4.6, 70))
     df = df.rename(columns={"jets_pt": pt_name, "jets_PartonTruthLabelID": label_name})
     ax = sns.histplot(data=df, x=pt_name, hue=label_name, weights="weight_spectrum", multiple="layer", log_scale=(
         False, True), bins=bins, common_norm=False, stat="count", palette='Set1', element="step", fill=False, hue_order=['quark', 'gluon'])
-    
+
     lss = ['-', '--']
     handles = ax.legend_.legendHandles[::-1]
     for line, ls, handle in zip(ax.lines, lss, handles):
         line.set_linestyle(ls)
         handle.set_ls(ls)
-        
+
     ax.legend_.set_title(None)
     plt.xlabel(r"Jet $p_{\mathrm{T}}$ [TeV]", horizontalalignment='right', x=1.0)
     plt.ylabel(r"Counts", horizontalalignment='right', y=1.0)
@@ -31,14 +33,14 @@ def sns_label_plotting(df):
         subtext='Simulation Preliminary \n 13 TeV',
         sub_font_size=10,
     )
-    plt.savefig("data/pythia_W_flat_70_JZ10/train/flat_pt.pdf", bbox_inches='tight')
-    plt.savefig("data/pythia_W_flat_70_JZ10/train/flat_pt.png", dpi=400, bbox_inches='tight')
+    plt.savefig(f"{save_path}/flat_pt.pdf", bbox_inches='tight')
+    plt.savefig(f"{save_path}/flat_pt.png", dpi=400, bbox_inches='tight')
     plt.cla()
     plt.clf()
     plt.close()
 
 
-def sns_label_plotting_noW(df):
+def sns_label_plotting_noW(df, save_path):
     pt_name = r"$p_{\mathrm{T}}$ [TeV]"
     label_name = "label"
     bins = list(np.linspace(0.06, 4.6, 70))
@@ -60,27 +62,27 @@ def sns_label_plotting_noW(df):
         subtext='Simulation Preliminary \n 13 TeV',
         sub_font_size=10,
     )
-    plt.savefig("data/pythia_W_flat_70_JZ10/train/flat_pt_noW.pdf", bbox_inches='tight')
-    plt.savefig("data/pythia_W_flat_70_JZ10/train/flat_pt_noW.png", dpi=400, bbox_inches='tight')
+    plt.savefig(f"{save_path}/flat_pt_noW.pdf", bbox_inches='tight')
+    plt.savefig(f"{save_path}/flat_pt_noW.png", dpi=400, bbox_inches='tight')
     plt.cla()
     plt.clf()
     plt.close()
 
 
-def sns_label_plotting_phys(df):
+def sns_label_plotting_phys(df, save_path):
     pt_name = r"$p_{\mathrm{T}}$ [TeV]"
     label_name = "label"
     df = df.rename(columns={"jets_pt": pt_name, "jets_PartonTruthLabelID": label_name})
     ax = sns.histplot(data=df, x=pt_name, hue=label_name, weights="weight", multiple="layer", log_scale=(
         False, True), bins=100, common_norm=False, stat="count", palette='Set1', element="step", fill=False, hue_order=['quark', 'gluon'])
     ax.legend_.set_title(None)
-    
+
     lss = ['-', '--']
     handles = ax.legend_.legendHandles[::-1]
     for line, ls, handle in zip(ax.lines, lss, handles):
         line.set_linestyle(ls)
         handle.set_ls(ls)
-        
+
     plt.xlabel(r"Jet $p_{\mathrm{T}}$ [TeV]", horizontalalignment='right', x=1.0)
     plt.ylabel(r"Counts", horizontalalignment='right', y=1.0)
     plt.ylim(1e-6, 6e12)
@@ -90,34 +92,44 @@ def sns_label_plotting_phys(df):
         subtext='Simulation Preliminary \n 13 TeV',
         sub_font_size=10,
     )
-    plt.savefig("data/pythia_physical/phys_pt.pdf", bbox_inches='tight')
-    plt.savefig("data/pythia_physical/phys_pt.png", dpi=400, bbox_inches='tight')
+    plt.savefig(f"{save_path}/phys_pt.pdf", bbox_inches='tight')
+    plt.savefig(f"{save_path}/phys_pt.png", dpi=400, bbox_inches='tight')
     plt.cla()
     plt.clf()
     plt.close()
 
+
 def load_dataframe(path):
-    ds = JIDENNDataset.load(path)
-    ds = ds.map(lambda x: {'jets_pt':x['jets_pt'], 'weight_spectrum':x['weight_spectrum'], 'jets_PartonTruthLabelID':x['jets_PartonTruthLabelID']}).to_pandas()
+    ds = JIDENNDataset.load(path).dataset
+    ds = ds.map(lambda x: {'jets_pt': x['jets_pt'], 'weight_spectrum': x['weight_spectrum'],
+                'jets_PartonTruthLabelID': x['jets_PartonTruthLabelID']}).to_pandas()
     return ds
+
 
 if __name__ == "__main__":
     HUE_MAPPER = {1: 'quark', 2: 'quark', 3: 'quark', 4: 'quark', 5: 'quark', 6: 'quark', 21: 'gluon'}
+    train_path = "data/pythia_W_flat_70_JZ10/train"
     try:
-        df = pd.read_csv("data/pythia_W_flat_70_JZ10/train/dataset.csv")
+        df = pd.read_csv(os.path.join(train_path, "dataset.csv"))
     except FileNotFoundError:
-        df = load_dataframe("data/pythia_W_flat_70_JZ10/train")
-        df.to_csv("data/pythia_W_flat_70_JZ10/train/dataset.csv")
+        df = load_dataframe(os.path.join(train_path, "dataset"))
+        df.to_csv(os.path.join(train_path, "dataset.csv"))
+
     df['jets_pt'] *= 1e-6
     df['weight_spectrum'] /= 1e4
     df = df[df['jets_pt'] > 0.07]
     df = df[df['jets_pt'] < 5]
     # remap the labels
     df['jets_PartonTruthLabelID'] = df['jets_PartonTruthLabelID'].replace(HUE_MAPPER)
-    sns_label_plotting(df)
-    sns_label_plotting_noW(df)
+    sns_label_plotting(df, train_path)
+    sns_label_plotting_noW(df, train_path)
+    phys_path = "data/pythia_physical"
 
-    df = pd.read_csv("data/pythia_physical/pythia_physical.csv")
+    try:
+        df = pd.read_csv(os.path.join(phys_path, "pythia_physical.csv"))
+    except FileNotFoundError:
+        df = load_dataframe(phys_path)
+        df.to_csv(os.path.join(phys_path, "pythia_physical.csv"))
     df['jets_pt'] *= 1e-6
     df['jets_PartonTruthLabelID'] = df['jets_PartonTruthLabelID'].replace(HUE_MAPPER)
-    sns_label_plotting_phys(df)
+    sns_label_plotting_phys(df, phys_path)
